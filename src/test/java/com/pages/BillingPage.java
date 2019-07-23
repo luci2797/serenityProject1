3f4fe4c20e57b7af3de711e07362701ffd1b66c8
GiurgiuDiana <giurgiudiana97@yahoo.com>
2019-07-23 12:59:32 +0300

    message

--- src/test/java/com/pages/BillingPage.java
+++ src/test/java/com/pages/BillingPage.java
@@ -1,4 +1,27 @@
 package com.pages;
 
-public class BillingPage {
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class BillingPage extends PageObject {
+
+    @FindBy(css="#billing-address-select")
+    private List<WebElementFacade>selectBillingAddress;
+
+    @FindBy(css="[title='Ship to this address']")
+    private WebElementFacade radioShipToThisAddress;
+
+    @FindBy(css="[title='Ship to different address']")
+    private WebElementFacade getRadioShipToDifferentAddress;
+
+    @FindBy(css="#billing-buttons-container >button")
+    private WebElementFacade continueBillingButton;
+
+    /////in momentul inj care dau continue retin adresa ca sa o pot verifica ca se noteaza cum trebuie
+
+
 }
